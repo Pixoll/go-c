cff8d20e2f88abfef10da814427b9e733c64e019
Pixoll <pixoll.gamer@gmail.com>
2023-06-04 17:37:15 -0400

    Custom input de texto

---
+++ src/util.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char *strget(char *buffer, int max) {
+    int size = 0;
+    for (int i = 0; i < max - 1; i++) {
+        char c;
+        scanf("%c", &c);
+        // detectó character inválido
+        if (c == '\0') c = '?';
+        if (c == '\n') break;
+        buffer[i] = c;
+        size++;
+    }
+
+    buffer[size] = '\0';
+    return buffer;
+}
---
+++ src/util.h
@@ -0,0 +1,1 @@
+char *strget(char *buffer, int size);
