2190cfefef03bd3a1984649c2ce2917abc041657
Pixoll <pixoll.gamer@gmail.com>
2023-06-04 16:59:58 -0400

    Archivo de manejo de datos

---
+++ src/datos.c
@@ -0,0 +1,64 @@
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define LEER "rb"
+#define ESCRIBIR "wb"
+#define LEER_ESCRIBIR "rb+"
+
+#define NOMBRE_MAX 32
+
+char cwd[PATH_MAX];
+char rutaArchivo[PATH_MAX];
+FILE *archivo;
+
+typedef struct Datos {
+    char nombre[NOMBRE_MAX];
+} Datos;
+
+Datos datos;
+
+void setupDatos() {
+    if (getcwd(cwd, PATH_MAX) == NULL) {
+        perror("getcwd() error");
+        exit(1);
+    }
+
+    strcpy(rutaArchivo, cwd);
+    const char *nombreArchivo = "/data.bin";
+    if (strlen(rutaArchivo) + strlen(nombreArchivo) > PATH_MAX) {
+        perror("exceded maximum path length");
+        exit(1);
+    }
+
+    strcat(rutaArchivo, nombreArchivo);
+    archivo = fopen(rutaArchivo, LEER);
+    if (archivo == NULL) {
+        archivo = fopen(rutaArchivo, ESCRIBIR);
+        fclose(archivo);
+        return;
+    }
+
+    fread(&datos, sizeof(datos), 1, archivo);
+    fclose(archivo);
+}
+
+Datos obtenerDatos() {
+    return datos;
+}
+
+void guardarDatos() {
+    archivo = fopen(rutaArchivo, LEER_ESCRIBIR);
+    const int saved = fwrite(&datos, sizeof(datos), 1, archivo);
+    if (!saved) {
+        perror("error while saving");
+        exit(1);
+    }
+    fclose(archivo);
+}
+
+void guardarNombre(char *nombre) {
+    strcpy(datos.nombre, nombre);
+    guardarDatos();
+}
---
+++ src/datos.h
@@ -0,0 +1,9 @@
+#define NOMBRE_MAX 32
+
+typedef struct Datos {
+    char nombre[NOMBRE_MAX];
+} Datos;
+
+void setupDatos();
+Datos obtenerDatos();
+void guardarNombre(char *nombre);
