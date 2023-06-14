99c116fb258ab7489ecfd3ab34add579811b71fa
igpadilla2021 <igpadilla2021@gmail.com>
2023-06-13 22:43:02 -0400

    se modifico Tablero_Max

--- src/tablero.c
+++ src/tablero.c
@@ -7,73 +7,73 @@
 
 GoPartida partida;
 
-void crearTablero(int size) {
+void crearTablero(int size)
+{
     partida.size = size;
-    const int max = size - 1;
+    const int max = size;
 
-    partida.tablero[0][0] = '+';
-    partida.tablero[0][max] = '+';
+    partida.tablero[1][1] = '+';
+    partida.tablero[1][max] = '+';
     partida.tablero[max][max] = '+';
-    partida.tablero[max][0] = '+';
+    partida.tablero[max][1] = '+';
 
-    for (int i = 1; i < max; i++) {
+    for (int i = 2; i < max; i++)
+    {
         partida.tablero[max][i] = '+';
-        partida.tablero[0][i] = '+';
-        partida.tablero[i][0] = L'¦';
+        partida.tablero[1][i] = '+';
+        partida.tablero[i][1] = L'¦';
         partida.tablero[i][max] = L'¦';
-        for (int j = 1; j < max; j++)
+        for (int j = 2; j < max; j++)
             partida.tablero[i][j] = L'¦';
     }
 }
 
-void printTablero() {
+void printTablero()
+{
     const int size = partida.size;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-            printf(j < size - 1 ? "%c-" : "%c", partida.tablero[i][j]);
+    for (int i = 1; i < size+1; i++)
+    {
+        for (int j = 1; j < size+1; j++)
+            printf(j+1 < size +1 ? "%c-" : "%c", partida.tablero[i][j]);
         printf("\n");
     }
     printf("\n");
 }
 
-void jugarTablero() {
-    printTablero();
+bool turnoNegras = true;
 
+void jugarTablero()
+{
+    printTablero();
+    int x, y;
     const int size = partida.size;
     int turno = 0;
-    bool turnoNegras = true;
     bool ocupadas[size][size];
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
+    for (int i = 1; i < size+1; i++)
+        for (int j = 1; j < size+1; j++)
             ocupadas[i][j] = false;
-
-    while (turno < 100) {  // condición de victoria
-        int x, y;
+    while (turno < 100)
+    { // condición de victoria
         printf("\tTurno de %s\n\n", turnoNegras ? "negras" : "blancas");
         printf("Insertar coordenada:\n");
         printf("x: ");
         scanf("%d", &x);
         printf("y: ");
         scanf("%d", &y);
-        x--;
-        y--;
 
-        while (ocupadas[x][y] == true) {
+        while (ocupadas[x][y] == true)
+        {
             printf("Esa casilla ya esta ocupada!\n");
             printf("x: ");
             scanf("%d", &x);
             printf("y: ");
             scanf("%d", &y);
-            x--;
-            y--;
         }
         ocupadas[x][y] = true;
-        partida.tablero[x][y] = turnoNegras ? 'O' : '@';
-
+        partida.tablero[x][y] = turnoNegras ? 'X' : 'O';
         limpiarConsola();
         printTitulo();
         printTablero();
-
         turnoNegras = !turnoNegras;
         turno++;
     }
