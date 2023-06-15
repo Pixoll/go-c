edade94215c44f7d6f72547c4ebff9563b423022
Pixoll <pixoll.gamer@gmail.com>
2023-06-15 18:52:03 -0400

    Usar enums

--- src/menu.h
+++ src/menu.h
@@ -2,16 +2,20 @@
 
 #define _MENU_DEFINIDO
 
-#define MENU_JUGAR 0
-#define MENU_REGLAS 1
-#define MENU_CONFIG 2
-#define MENU_STATS 3
 #define MENUS 4
+enum MENU {
+    MENU_JUGAR,
+    MENU_REGLAS,
+    MENU_CONFIG,
+    MENU_STATS,
+};
 
-#define CONFIG_NOMBRE 0
-#define CONFIG_BORRAR_PARTIDAS 1
-#define CONFIG_BORRAR_TODO 2
 #define CONFIGS 3
+enum MENU_CONFIG {
+    CONFIG_NOMBRE,
+    CONFIG_BORRAR_PARTIDAS,
+    CONFIG_BORRAR_TODO,
+};
 
 #define VOLVER -1
 #define SALIR -2
