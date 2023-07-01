bf64189965aa02d95cf705842ee3268e92bd9861
Pixoll <pixoll.gamer@gmail.com>
2023-07-01 16:51:51 -0400

    Actualizar build.bat

--- build.bat
+++ build.bat
@@ -4,13 +4,25 @@ setlocal EnableDelayedExpansion
 set exename=%*
 if [!exename!] == [] set exename=go
 
-cd src
-set files=
-for /f "tokens=*" %%f in ('dir /s /b *.c') do (
-    set files=!files! "%%f"
+set files_common=
+for /f "tokens=*" %%f in ('dir src\common\*.c /s/b') do (
+    set files_common=!files_common! "%%f"
 )
 
-cd ..
-set command=gcc -g !files! -o "!exename!.exe"
-echo Running: %command%
-%command%
+set files_console=!files_common!
+for /f "tokens=*" %%f in ('dir src\console\*.c /s/b') do (
+    set files_console=!files_console! "%%f"
+)
+
+set command_console=gcc -g !files_console! -o "!exename!-console.exe"
+echo Running: %command_console%
+%command_console%
+
+rem set files_sdl=!files_common!
+rem for /f "tokens=*" %%f in ('dir src\sdl\*.c /s/b') do (
+rem     set files_sdl=!files_sdl! "%%f"
+rem )
+
+rem set command_sdl=gcc -g !files_sdl! -o "!exename!-sdl.exe"
+rem echo Running: %command_sdl%
+rem command_sdl%
